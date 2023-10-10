af00839ff5c8c0f229dcc38be8f6f39568d278d4
Paweł Drożański <pawdro@gmail.com>
2023-10-10 20:51:48 +0200

    CRUD based on repository

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -23,7 +23,7 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: number) {
     return this.coffeesService.findOne(id);
   }
 
@@ -34,7 +34,7 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     this.coffeesService.update(id, updateCoffeeDto);
   }
 
