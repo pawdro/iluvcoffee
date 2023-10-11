ce59fde5096fa14e914253f06a906cee75df0afc
Paweł Drożański <pawdro@gmail.com>
2023-10-11 13:52:22 +0200

    Relation between 2 entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesModule } from './coffees/coffees/coffees.module';
+import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
